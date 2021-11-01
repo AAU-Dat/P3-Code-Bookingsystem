0d10c947e9bcda1bcf608318dba7bdb9b07fc977
ARKAEA <rkacso20@student.aau.dk>
2021-11-01 14:03:04 +0100

    Added formular

---
+++ Reservations-system/Data/ReservationClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Reservation
+{
+
+    public DateTime StartDate { get; set;} = DateTime.Now;
+
+    public DateTime FinishDate { get; set; } = DateTime.Now;
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Mail { get; set; }
+
+    public string AccountNumber { get; set; }
+
+    public string RegistrationNumber { get; set; }
+
+    public bool Cleaning { get; set; } = false;
+
+    public bool IsOver18 { get; set; }
+
+    public string OtherInfo { get; set; }
+
+}
+
