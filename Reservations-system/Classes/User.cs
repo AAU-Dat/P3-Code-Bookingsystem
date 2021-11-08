4905ae32fd31e6e1765566785d7d8fbb596f67f0
ironmand123 <74530153+ironmand123@users.noreply.github.com>
2021-11-08 12:49:28 +0100

    login

---
+++ Reservations-system/Classes/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservations_system.Classes
+{
+    public class User
+    {
+        private string _userName;
+        private string _password;
+        private string _email;
+
+        public User(string userName, string password, string email)
+        {
+            Email = email;
+            Password = password;
+            UserName = userName;
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+
+    }
+}
