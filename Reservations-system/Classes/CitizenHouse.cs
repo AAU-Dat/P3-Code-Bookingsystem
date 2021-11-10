af448cbc2c584a0c01f88a0d7cf0082fa99cd44d
ARKAEA <rkacso20@student.aau.dk>
2021-11-10 12:14:52 +0100

    --NOTE-- DO NOT MERGE Added some new classes. The branch currently has feastures that do not work, and crash the program.

---
+++ Reservations-system/Classes/CitizenHouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Reservations_system.Classes;
+
+namespace Reservations_system.Classes
+{
+    public class CitizenHouse
+    {
+        public string Address { get; set; }
+
+        public bool IsInUse { get; set; }
+    }
+}
---
+++ Reservations-system/Classes/Reservation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Reservations_system.Classes
+{
+
+    public class Reservation
+    {
+        public Reservation()
+        {
+
+        }
+        public Reservation(DateTime startDate, DateTime finishDate, string firstName, string lastName, string phoneNumber,
+                            string mail,string accountNumber, string registrationNumber, bool cleaning, bool isOver18, string otherInfo, int iD)
+        {
+            StartDate = startDate;
+            FinishDate = finishDate;
+            Cleaning = cleaning;
+            OtherInfo =otherInfo;
+            ID = iD;
+
+            Guest guest = new Guest();
+        }
+        [Required]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime FinishDate { get; set; } = DateTime.Now.AddDays(1);
+
+        public bool Cleaning { get; set; } = false;
+
+        public string OtherInfo { get; set; }
+
+        public int ID { get; set; }
+
+
+    }
+
+
+}
