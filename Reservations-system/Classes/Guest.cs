fe1e00b0846dd33a172aaaa7bbc97b77e0a565c9
ARKAEA <rkacso20@student.aau.dk>
2021-11-10 12:14:52 +0100

    --NOTE-- DO NOT MERGE Added some new classes. The branch currently has feastures that do not work, and crash the program.

---
+++ Reservations-system/Classes/Guest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservations_system.Classes
+{
+    public abstract class Guest
+    {
+
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public string PhoneNumber { get; set; }
+
+
+        public string Mail { get; set; }
+
+
+        public string Address { get; set; }
+
+
+        public string AccountNumber { get; set; }
+
+
+        public string RegistrationNumber { get; set; }
+
+
+        public bool IsOver18 { get; set; }
+
+    }
+}
