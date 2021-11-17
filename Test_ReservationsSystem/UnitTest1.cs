cfde4aa55b61a3144f9f5c83ea3cc53e4ee8b089
AHougaard <74406435+AHougaard@users.noreply.github.com>
2021-11-17 10:45:57 +0100

    added test project and started implimenting bunit tests (step 3 to the guide)

---
+++ Test_ReservationsSystem/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test_ReservationsSystem
+{
+    public class KalenderTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
