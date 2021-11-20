468e598229dd38d8cad07c406b51244c01988031
ARKAEA <rkacso20@student.aau.dk>
2021-11-20 12:08:08 +0100

    Added some validation for the class Reservaiton. The validation is not done

--- Reservations-system/Classes/Renter.cs
+++ Reservations-system/Classes/Renter.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Reservations_system.Classes
 {
-    
+
     public abstract class Renter
     {
         private string _name;
@@ -31,21 +32,69 @@ namespace Reservations_system.Classes
         public string Name
         {
             get { return _name; }
-            set { _name = value; }
+            set
+            {
+                if (String.IsNullOrEmpty(_name))
+                {
+                    throw new ArgumentNullException("String Name is null or missing");
+                }
+                if (_name.Any(char.IsDigit))
+                {
+                    throw new ArgumentException("String Name contains digits");
+                }
+                if (_name.Length > 40 && _name.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException("String Name's length is invalid");
+                }
+                _name = char.ToUpper(value[0]) + value.Substring(1);
+            }
         }
 
         public string PhoneNumber
         {
             get { return _phoneNumber; }
-            set { _phoneNumber = value; }
+            set
+            {
+                if (_phoneNumber.Length != 8)
+                {
+                    throw new ArgumentOutOfRangeException("String PhoneNumber's length is invalid");
+                }
+
+                if (String.IsNullOrEmpty(_phoneNumber))
+                {
+                    throw new ArgumentNullException("String PhoneNumber is null or missing");
+                }
+
+                if (_phoneNumber.Any(char.IsLetter))
+                {
+                    throw new ArgumentException("String Phonenumber contains a letter");
+                }
+
+                _phoneNumber = value;
+
+            }
         }
 
         public string Mail
         {
             get { return _mail; }
-            set { _mail = value; }
-        }
+            set {
+                if (String.IsNullOrEmpty(_mail))
+                {
+                    throw new ArgumentNullException("String Mail is null or misisng");
+                }
 
+                if(ValidateMailAdressFormat(_mail))
+                {
+                    _mail = value;
+                }
+
+            }
+        }
+        public bool ValidateMailAdressFormat(string mailToBeValidated)
+        {
+            MailAddress m = MailAddress(mailToBeValidated);
+        }
         public string Address
         {
             get { return _address; }
