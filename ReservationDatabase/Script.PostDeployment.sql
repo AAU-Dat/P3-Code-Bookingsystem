6340f64280a23ceba0be02496ef93e2b91af53b4
Daniel Runge Petersen <dpet20@student.aau.dk>
2021-12-01 08:39:10 +0100

    post deployment script template removed.

--- ReservationDatabase/Script.PostDeployment.sql
+++ ReservationDatabase/Script.PostDeployment.sql
@@ -1,15 +1,4 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
-IF NOT EXISTS (SELECT 1 FROM [dbo].[Guest])
+﻿IF NOT EXISTS (SELECT 1 FROM [dbo].[Guest])
 BEGIN
     INSERT INTO [dbo].[Guest] ([Name], [Address], [Email], [Phone], [AccountNumber])
     VALUES 
