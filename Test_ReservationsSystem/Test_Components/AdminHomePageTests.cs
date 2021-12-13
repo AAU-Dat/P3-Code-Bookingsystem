dd6806397ee1c1614ea182ae7e8a6628512cba0a
ARKAEA <rkacso20@student.aau.dk>
2021-12-13 12:46:32 +0100

    Created some tests

---
+++ Test_ReservationsSystem/Test_Components/AdminHomePageTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Bunit;
+using Reservations_system.Components;
+
+namespace Test_ReservationsSystem.Test_Components
+{
+    public class AdminHomePageTests
+    {
+        [Fact]
+        public void TestEditModeValue()
+        {
+            using var ctx = new TestContext();
+            var componentTest = ctx.RenderComponent<AdminHomePage>().Instance;
+
+
+            var expectedEditModeValue = false;
+
+            var actualEditModeValue = componentTest.editMode;
+
+            Assert.Equal(expectedEditModeValue, actualEditModeValue);
+        }
+
+        [Fact]
+        public void PrintNothing()
+        {
+            string dum = "yes";
+            Assert.Equal("yes", dum);
+        }
+    }
+}
